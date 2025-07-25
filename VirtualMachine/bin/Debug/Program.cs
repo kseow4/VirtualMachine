8398592d606b7cc31fa66d0a0d2c975c5ea95eac
Kyle Seow <kjc801@gmail.com>
2025-07-24 21:53:18 -0600

    Initial commit

---
+++ VirtualMachine/bin/Debug/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using VirtualMachine.VMTools;
+
+namespace VirtualMachine
+{
+    public static class Program
+    {
+        public static void Main(params string[] args)
+        {
+            try
+            {
+                VirtualMachine vm = new VirtualMachine(args.Length > 1 ? args[1] : "Resources/proj4.asm");
+                //  vm.Assembler.Run(args.Length > 1 ? args[1] : "Resources/test_asm.txt");
+
+                vm.Run();
+                LoggingService.WriteLogs();
+            }
+            catch (Exception)
+            {
+                LoggingService.WriteLogs();
+               //using (StreamWriter ofile = new StreamWriter(@".\error.txt")) { ofile.WriteLine(e.Message.ToString()); }
+            }
+        }
+    }
+}
---
+++ VirtualMachine/bin/Release/Resources/test1.asm
@@ -0,0 +1,61 @@
+﻿# Check even or odd:
+
+		TRP		2
+		MOV		R1		R3
+		TRP		1		
+		LDB		R3		newLine
+		TRP		3
+		TRP		4
+		MOV		R2		R3
+		TRP		3
+
+
+
+
+#		ADI		R4		-5
+#		ADI		R1		-5
+#		ADI		R2		3
+#		LDR		R6		modtwo		
+#		DIV		R1		R2			
+#		MUL		R1		R2				
+#		MOV		R3		R1
+#		TRP		1
+
+
+
+		LDB		R3		newLine
+		TRP		3
+		TRP		0
+
+
+i		.INT	0
+sum		.INT	0
+temp	.INT	0
+result	.INT	0
+SIZE	.INT	10
+ARR		.INT	0
+		.INT	1
+		.INT	2
+		.INT	3
+		.INT	4
+		.INT	5
+		.INT	6
+		.INT	7
+		.INT	8
+		.INT	9
+
+modtwo	.INT	2
+iBB		.INT	0
+iBA		.INT	0
+iAC		.INT	102
+iAD		.INT	7
+iA		.INT	3
+iB		.INT	2
+ii		.BYT	'i'
+ss		.BYT	's'
+nn		.BYT	'n'
+vv		.BYT	'v'
+ee		.BYT	'e'
+comma	.BYT	','
+space	.BYT	' '
+newLine	.BYT	'\r'
